b8ab24fcff2afa01f6387c1b6dbf2df9248f8890
Aitor Garmendia <aitor.garmendia@mobility.sener>
2026-03-10 13:21:36 +0100

    Base project

---
+++ Source/DRIVE_Template.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class DRIVE_TemplateTarget : TargetRules
+{
+	public DRIVE_TemplateTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("DRIVE_Template");
+	}
+}
---
+++ Source/DRIVE_Template/DRIVE_Template.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DRIVE_Template.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DRIVE_Template, "DRIVE_Template" );
